cdf181690021121e62c4d13300fc7bfbffbd04b3
Sidney So <buffalo704@hotmail.com>
2018-07-12 02:20:30 -0400

    Left Rotation
    https://www.hackerrank.com/challenges/array-left-rotation/problem

---
+++ Data Structures/Arrays/Left Rotation/Solution.js
@@ -0,0 +1,52 @@
+// https://www.hackerrank.com/challenges/array-left-rotation/problem
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function leftRotate(a, d) {
+    let count = 0;
+
+    while (count < d) {
+        const v = a.shift();
+        a.push(v);
+        count++;
+    }
+
+    return a.join(' ');
+}
+
+function main() {
+    const nd = readLine().split(' ');
+
+    const n = parseInt(nd[0], 10);
+
+    const d = parseInt(nd[1], 10);
+
+    const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = leftRotate(a,d);
+
+    console.log(result);
+
+}
