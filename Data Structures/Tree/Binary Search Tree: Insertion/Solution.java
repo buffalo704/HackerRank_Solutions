c1d8384363e4710a3ec32c4e0e36a0fcd3a36a8c
Sidney So <buffalo704@hotmail.com>
2018-07-14 01:15:58 -0400

    Binary Search Tree: Insertion
    https://www.hackerrank.com/challenges/binary-search-tree-insertion/problem

---
+++ Data Structures/Tree/Binary Search Tree: Insertion/Solution.java
@@ -0,0 +1,102 @@
+// https://www.hackerrank.com/challenges/binary-search-tree-insertion/problem
+
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+    public static void preOrder( Node root ) {
+
+        if( root == null)
+            return;
+
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+
+    }
+
+     /* Node is defined as :
+     class Node
+        int data;
+        Node left;
+        Node right;
+
+    */
+
+    public static Node insert(Node root,int data) {
+        Node node = new Node(data);
+
+        if (root == null) {
+            return node;
+        }
+
+        Node currentNode = root;
+
+        while (currentNode != null) {
+
+            if (data < currentNode.data) {
+
+                if (currentNode.left == null) {
+                    currentNode.left = node;
+                    break;
+                }
+                currentNode = currentNode.left;
+
+            } else if (data > currentNode.data) {
+
+                if (currentNode.right == null) {
+                    currentNode.right = node;
+                    break;
+                }
+                currentNode = currentNode.right;
+            }
+        }
+
+        return root;
+    }
+
+    public static void bfs(Node root) {
+        Deque deque = new  LinkedList<Node>();
+
+        deque.push(root);
+
+        while (deque.size() > 0) {
+            Node node = (Node) deque.pop();
+            System.out.print(node.data + " ");
+
+            if (node.left != null) {
+                deque.push((Node) node.left);
+            }
+
+            if (node.right != null) {
+                deque.push((Node) node.right);
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        preOrder(root);
+    }
+}
